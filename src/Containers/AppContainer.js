548e2c2294c4e85a27d98c7395ef3fccd652d958
Ekansh Jain <ekanshjain1999@gmail.com>
2020-04-04 22:12:25 +0530

    Changed paths

--- src/Containers/AppContainer.js
+++ src/Containers/AppContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Search from './Components/Search'
-import TrackList from './Components/TrackList'
-import Player from './Components/Player'
+import Search from '../Components/Search'
+import TrackList from '../Components/TrackList'
+import Player from '../Components/Player'
 
 class AppContainer extends React.Component{
 	constructor(){
